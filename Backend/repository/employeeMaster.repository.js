635f3dd323002658ca7ca4aad869f1382501a19c
akshayparishram <akshayparishram@gmail.com>
2025-03-15 23:05:38 +0530

    entitlement routs updated

--- Backend/repository/employeeMaster.repository.js
+++ Backend/repository/employeeMaster.repository.js
@@ -1,30 +1,33 @@
-const { Op, Sequelize } = require('sequelize');
-const employeeMaster = require("../models/employeeMaster.model.js");
-const Entitlement=require("../models/employeeEntitlement.model.js")
-const CustomError = require("../utils/errorHandler.util.js");
-const { executeQuery } = require('../utils/dbhelper.util.js');
+const { Op, Sequelize } = require( 'sequelize' );
+const employeeMaster = require( "../models/employeeMaster.model.js" );
+const Entitlement = require( "../models/employeeEntitlement.model.js" )
+const CustomError = require( "../utils/errorHandler.util.js" );
+const { executeQuery } = require( '../utils/dbhelper.util.js' );
 
-const moment = require('moment');
+const moment = require( 'moment' );
 
-const findAll = async (page,pageSize) => {
+const findAll = async ( page, pageSize ) =>
+{
 
-    const offset = (page - 1) * pageSize;
-    try {
-        const { count, rows }= await employeeMaster.findAndCountAll({
+    const offset = ( page - 1 ) * pageSize;
+    try
+    {
+        const { count, rows } = await employeeMaster.findAndCountAll( {
             limit: pageSize,
             offset: offset,
-            order: [['EmpId1', 'ASC']]
+            order: [ [ 'EmpId1', 'ASC' ] ]
 
-          });
-            // Count employees who have left
-        const leftCount = await employeeMaster.count({
+        } );
+        // Count employees who have left
+        const leftCount = await employeeMaster.count( {
             where: { hasLeft: true }
-        });
+        } );
 
-          return {count,rows,leftCount};
-    } catch (error) {
-        console.error("Database Error :", error);
-        throw new CustomError(500, "Database error occurred during getting all data from employee master.");
+        return { count, rows, leftCount };
+    } catch ( error )
+    {
+        console.error( "Database Error :", error );
+        throw new CustomError( 500, "Database error occurred during getting all data from employee master." );
     }
 };
 // const { count, rows } = await YourModel.findAndCountAll({
@@ -38,109 +41,144 @@ const findAll = async (page,pageSize) => {
 //     order: [['id', 'ASC']]
 //   });
 
-const findOne = async (empid) => {
-    try {
-        return employee= await employeeMaster.findOne({ where: { EmpID: empid } });
-    } catch (error) {
-        throw new Error('Error in fetching employee by id: ' + error.message);
+const findOne = async ( empid ) =>
+{
+    try
+    {
+        return employee = await employeeMaster.findOne( { where: { EmpID: empid } } );
+    } catch ( error )
+    {
+        throw new Error( 'Error in fetching employee by id: ' + error.message );
     }
 };
-const getAllByBranch = async (branchCode) => {
-    try {
-        const employees = await employeeMaster.findAll({
+const getAllByBranch = async ( branchCode ) =>
+{
+    try
+    {
+        const employees = await employeeMaster.findAll( {
             where: {
                 branchCode: branchCode,
-                [Op.and]: [
-                    Sequelize.literal("ISNULL(IsBilled, 0) = 1"),
-                    Sequelize.literal("ISNULL(hasLeft, 0) = 1")
+                [ Op.and ]: [
+                    Sequelize.literal( "ISNULL(IsBilled, 0) = 1" ),
+                    Sequelize.literal( "ISNULL(hasLeft, 0) = 1" )
                 ]
             },
-        });
+        } );
         return employees;
-    } catch (error) {
-        throw new Error('Error in fetching employee by branch: ' + error.message);
+    } catch ( error )
+    {
+        throw new Error( 'Error in fetching employee by branch: ' + error.message );
     }
 };
-const updateEmployee = async (employee) => {
-    console.log("Employee to update:", employee); 
+const updateEmployee = async ( employee ) =>
+{
+    console.log( "Employee to update:", employee );
     const empid = employee.EmpID;
 
-    try {
-        return await employeeMaster.update(employee, { where: { EmpID: empid } });
-    } catch (error) {
-        console.error("Update failed:", error); 
-        throw new Error('Error in updating employee: ' + error.message);
+    try
+    {
+        return await employeeMaster.update( employee, { where: { EmpID: empid } } );
+    } catch ( error )
+    {
+        console.error( "Update failed:", error );
+        throw new Error( 'Error in updating employee: ' + error.message );
     }
 };
 
-const createEmployee = async (employee) => {
-    const empid=employee.EmpID;
-    try {
-        return employee= await employeeMaster.create(employee);
-    } catch (error) {
-        throw new Error(' ! error in saving employee : ' + error.message);
+const createEmployee = async ( employee ) =>
+{
+    const empid = employee.EmpID;
+    try
+    {
+        return employee = await employeeMaster.create( employee );
+    } catch ( error )
+    {
+        throw new Error( ' ! error in saving employee : ' + error.message );
     }
 };
-const findOneEntitle = async (empid) => {
+const findOneEntitle = async ( empid ) =>
+{
 
-    console.log(`Type of empid: ${typeof empid}`);
+    console.log( `Type of empid: ${ typeof empid }` );
 
-    if (!empid) throw new Error('Employee ID is required');
+    if ( !empid ) throw new Error( 'Employee ID is required' );
 
-    console.log(`Fetching entitlement for empid: ${empid}`);
+    console.log( `Fetching entitlement for empid: ${ empid }` );
 
-    const query = `SELECT * FROM EmployeeEntitlement WHERE EmpCode = '${empid}'`;
+    const query = `SELECT * FROM EmployeeEntitlement WHERE EmpCode = '${ empid }'`;
 
-    const result = await executeQuery(query);
+    const result = await executeQuery( query );
 
-    console.log(`Entitlement length: ${result.length}`);
-
+    console.log( `Entitlement length: ${ result.length }` );
     return result;
 
 };
 
-const createEntitle = async (entitlements) => {
-    try {
-        await Promise.all(
-            entitlements.map(async (entitle, index) => {
-                const { EmpCode, SalHead, FixedAmount } = entitle;
+const createEntitle = async ( entitlements ) =>
+{
+    try
+    {
+        let snoCounter = 1;
+        for ( const entitle of entitlements )
+        {
+            const { EmpCode, SalHead, FixedAmount } = entitle;
 
-                // Check if the record exists
-                const existingEntitlement = await Entitlement.findOne({
-                    where: { EmpCode, SalHead }
-                });
+            // Check if the record exists
+            const existingEntitlement = await Entitlement.findOne( {
+                where: { EmpCode, SalHead }
+            } );
 
-                if (existingEntitlement) {
-                    // Update FixedAmount for existing record
-                    await existingEntitlement.update({ FixedAmount });
-                } else {
-                    //gittng the all value from salaryheadmaster
+            if ( existingEntitlement )
+            {
+                // Update existing record
+                await Entitlement.update( {
+                    FixedAmount,
+                    Entitle: FixedAmount,
+                    changedDate: new Date().toISOString()
+                }, {
+                    where: {
+                        EmpCode: EmpCode,
+                        SalHead: SalHead
+                    }
+                } );
+                console.log( `Updated: ${ EmpCode } - ${ SalHead }` );
+            } else
+            {
+                // gittng the all value from salaryheadmaster
 
-                    // Insert new record with default values
-                    await Entitlement.create({
+                const result = await executeQuery( `SELECT * FROM SalaryHeadMaster WHERE Code = '${ SalHead }'` );
+                if ( result )
+                {
+                    //const EarningDeduction = result?.[0]?.EarningDeduction?.toLowerCase() !== 'earning';
+
+                    await Entitlement.create( {
                         EmpCode,
-                        sno: index + 1, // You can adjust logic for sno if needed
+                        sno: snoCounter++,
                         SalHead,
-                        isEditable: true, // Default value
+                        isEditable: true,
                         FixedAmount,
-                        Entitle: FixedAmount, // Default to FixedAmount
-                        changedDate: new Date(), // Default to current date
-                        Remarks: "Auto-generated entry", // Default remark
-                        EntCatg: "Salary", // Default value
-                        Type: "Earning", // Default value
-                        Deduction: 0, // Default value
-                        LedgerCode: "DEFAULT001" // Default value
-                    });
+                        Entitle: FixedAmount,
+                        changedDate: new Date().toISOString(),
+                        Remarks: " ",
+                        EntCatg: "SCD00001",
+                        Type: result[ 0 ]?.Description || " ",
+                        Deduction: result?.[ 0 ]?.EarningDeduction?.toLowerCase() === 'earning' ? 0 : FixedAmount,
+                        LedgerCode: "NULL"
+                    } );
+                } else
+                {
+                    console.error( 'Error in finding salary head ', error );
+                    return false;
                 }
-            })
-        );
-
-        return true;
-    } catch (error) {
-        console.error('Error in repository:', error);
-        return false;
+                console.log( `Created: ${ EmpCode } - ${ SalHead }` );
+            }
+        }
+    } catch ( error )
+    {
+        console.error( 'Error saving/updating entitlements:', error );
     }
-};
+}
+
 
 // const saveEmployee = async (employee) => {
 //     const empid=employee.EmpID;
